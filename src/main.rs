8c5b1db28464ea1da9c29229e43d3e6b3f537fd8
Merlin Addams <merlin.addams@protonmail.com>
2023-07-25 05:16:31 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,31 @@
-fn main() {}
+use filesystem::{FileSystem, OsFileSystem};
+use sysinfo::{DiskExt, System, SystemExt};
+
+fn bytes_to_gigabytes(bytes: u64) -> u64 {
+    bytes / (1024 * 1024 * 1024)
+}
+
+fn main() {
+    let mut sys = System::new_all();
+
+    sys.refresh_disks();
+
+    let fs = OsFileSystem::new();
+
+    sys.disks().iter().for_each(|disk| {
+        println!("{:?}", disk.mount_point());
+        println!(
+            "{:?} / {:?}",
+            bytes_to_gigabytes(disk.available_space()),
+            bytes_to_gigabytes(disk.total_space())
+        );
+
+        let path = disk.mount_point().to_path_buf();
+        fs.set_current_dir(path).unwrap();
+
+        let a = fs.read_dir(".").unwrap();
+        a.for_each(|b| {
+            println!("{}", b.unwrap().file_name().to_str().unwrap());
+        });
+    })
+}
